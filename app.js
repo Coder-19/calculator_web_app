7e626eceed1175000105cc45c3602fc443c92132
Coder-19 <arorajasman19@gmail.com>
2022-10-01 21:23:22 +0530

    adding the files to the repository

---
+++ app.js
@@ -0,0 +1,20 @@
+// adding the express module 
+const express = require('express');
+
+// creating an instance of express 
+const app = express();
+
+const portNumber = 3000;
+
+// using the get() method from the app instance to create a "/" root route for the app
+app.get("/",function(req,res){
+    // using the send() method from the res instance for sending a response when the user makes
+    // a request at "/" route
+    res.send("Home Page");
+});
+
+
+// using the listen() method from app instance for creating a server to listen at port 3000
+app.listen(portNumber, function(){
+    console.log("The server is up and running at "+portNumber);
+});
