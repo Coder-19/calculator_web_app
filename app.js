e012788fa447c0ea008d48db107e6875dda4127d
Coder-19 <arorajasman19@gmail.com>
2022-10-01 21:53:58 +0530

    creating an html form and sending that form as response to the
    user when the user goes to the root route

--- app.js
+++ app.js
@@ -8,9 +8,20 @@ const portNumber = 3000;
 
 // using the get() method from the app instance to create a "/" root route for the app
 app.get("/",function(req,res){
+    
+    console.log("The current directory path is:");
+    // using the __dirname for getting the path where our current project directory is present
+    console.log(__dirname);
+
     // using the send() method from the res instance for sending a response when the user makes
     // a request at "/" route
-    res.send("Home Page");
+    // res.send("Home Page");
+
+    // using the sendFile() method to send the index.html file to the user when the user hits
+    // the "/" root route
+    res.sendFile(__dirname + "/index.html");   // here we are using the __dirname variable from
+    // node for getting the current directory path and concatinating the name of the file
+    // that we have to send
 });
 
 
